41bc3d0216c407a1840df94aef8f240eb6cbacd4
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2025-12-30 21:32:46 +0700

    Implement user search and fetch in search.js
    Add user search and fetch functionality in search.js

---
+++ cmd/search.js
@@ -0,0 +1,5 @@
+await client.users.search("zuck", 10) // format "username", how many you fetching 
+
+await client.users.fetch(1) // fetch that specific users
+
+//here's the fun part, dont tell zuck they prob fired me (jk i dont even work at meta) 
