6e0320f628d119b7c3a12f314a2da3d3624ae97c
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2025-12-30 21:30:01 +0700

    Implement reply feature in reply.js
    Add functionality to reply to a post in JavaScript.

---
+++ cmd/reply.js
@@ -0,0 +1,2 @@
+await client.posts.reply(1, { contents: "Hello World!", post: "I replying this thread with javascript fr" })
+//yes it can reply a message, and funfact you can made a GROK rip-off on threads using this
