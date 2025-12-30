4a7bf2c59a01ba3f4bac923ab2ca543bc093923f
Rizky Daffy Al Fajr <162791897+RizkyDaffy@users.noreply.github.com>
2025-12-30 21:23:50 +0700

    Add post creation code in create.js
    Implement post creation functionality in create.js

---
+++ cmd/create.js
@@ -0,0 +1,2 @@
+await client.posts.create(1, { contents: "Hello World!" })
+// make a new post (it can be used with webhook ex: if there an ambert allert the webhook tells this code to create a post bout it)
